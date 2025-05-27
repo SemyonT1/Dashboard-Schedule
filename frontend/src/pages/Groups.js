f365eae1297cefa9800eaea8effb493ede4d1273
sam <emelanstepanovic56@gmail.com>
2025-05-27 23:44:17 +0300

    Add: .dockerignore DOckerFile.frontend

---
+++ frontend/src/pages/Groups.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Groups = () => {
+  return (
+    <div>
+      <h1>Группы</h1>
+      <p>Здесь будет аналитика групп</p>
+    </div>
+  );
+};
+
+export default Groups;
